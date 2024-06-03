47fa585be040c1dca7768f1a6b74ecaa463f16cb
luangalvaoxD <luanestudos98@gmail.com>
2024-06-03 04:32:23 -0300

    Adicionar Contador

---
+++ projeto-java/src/edu/Luan/primeirasemana/ContandoCarneiros.java
@@ -0,0 +1,12 @@
+package edu.Luan.primeirasemana;
+
+public class ContandoCarneiros {
+    public static void main(String[] args) {
+        for(int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando Carneirinhos " + carneirinhos);
+        }
+
+        System.out.println("Joãozinho foi de beise");
+    }
+    
+}
